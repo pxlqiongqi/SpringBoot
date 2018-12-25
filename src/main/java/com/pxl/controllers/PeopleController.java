c1024948ec7f7bfc1673f3f58c922fde2dea9fc7
pxlqiongqi <251624919@qq.com>
2018-12-25 22:17:48 +0800

    1

--- src/main/java/com/pxl/controllers/PeopleController.java
+++ src/main/java/com/pxl/controllers/PeopleController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
