9780cf7050f2e89fbf8feedac48f9d2422e2fb18
pxlqiongqi <251624919@qq.com>
2020-06-13 18:20:03 +0800

    commit

--- src/test/java/com/example/demo/ApplicationTests.java
+++ src/test/java/com/example/demo/ApplicationTests.java
@@ -1,9 +1,16 @@
 package com.example.demo;
 
+import com.sun.jmx.snmp.tasks.ThreadService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +18,10 @@ public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		ArrayList<String> name=new ArrayList<String>();
+		name.forEach((b)->{
+			System.out.println(b);
+		});
 	}
 
 }
